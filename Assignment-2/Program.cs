027f2758c64ce7f749c86b923e76640d7a91b640
visualstudio <visualstudio@122-PC14>
2019-03-06 10:28:29 -0500

    Lab 1 Commit

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -6,17 +6,12 @@ using System.Threading.Tasks;
 
 namespace Assignment_2
 {
-    public class program{
-        public static void Main()
-        {
-            DelegateExercises a = new DelegateExercises();
-            a.Method2();
-        }
-    }
+
+
+
+
     public class DelegateExercises
     {
-        // Student Name: Jatinder Singh  Student ID: C0725866
-        // Student Name: Arshdeep Singh Student ID: C0730385
 
         public delegate void MyDelegate();
 
