a36b12d80ceeabce5a08275c7f87ed5540b7cbb9
visualstudio <visualstudio@122-PC14>
2019-03-06 10:07:05 -0500

    Lab 1

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Assignment_2
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public class program{
+        public static void Main()
         {
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+        }
+    }
+    public class DelegateExercises
+    {
+        // Student Name: Jatinder Singh  Student ID: C0725866
+        // Student Name: Arshdeep Singh Student ID: C0730385
+
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
