6178e122e66c70af65639982e39e8eb26a3f0037
visualstudio <visualstudio@122-PC14>
2019-03-06 11:30:48 -0500

    Lab 07:  We are running the Method4 and giving five values to Method1 and Method2 as argument using for loop and getting result accordingly

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -33,16 +33,18 @@ namespace Assignment_2
 
         int Method1(int intMethod1)
         {
-            return intMethod1 * 2;
+            return intMethod1 * 4;
         }
         int Method2(int intMethod1)
         {
-            return intMethod1 * 10;  
+            return intMethod1 * 20;  
         }
         public void Method4(MyDelegate myDelegate)
         {
-            int result = myDelegate(10);
-            Console.WriteLine(result);
+            for(int i =1; i <=5; i++)
+            {
+                System.Console.Write(myDelegate(i) + " ");
+            }
         }
         public void Method3()
         {
